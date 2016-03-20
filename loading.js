6f6cd26124444231616702b8184af6198f1216f9
Scott Lewis <scooterpsu@gmail.com>
2016-03-19 21:53:46 -0400

    Moved some things, prep for game info

--- loading.js
+++ loading.js
@@ -15,19 +15,14 @@ function updateProgress(progress) {
 dew.on("show", function (event) {
     mapName = event.data.map || "";
     if (mapData[mapName]) {
-        $("body").css("background","black");
-        $(".container").css("border-width","1px 0 1px 0");
-        $(".container").css("background-image","url('maps/"+mapName+".png')");
+        $("body").css("background-image","url('maps/"+mapName+".png')");
         $("#title").text(mapData[mapName].name);
         $("#desc").text(mapData[mapName].desc);
         $(".serverMessage").text(serverMessage);
         $(".header").show();
         $(".footer").show();
     } else {
-        $("body").css("background","url('background.png') center");
-        $("body").css("background-size","cover");
-        $(".container").css("border-width","0");
-        $(".container").css("background-image","none");
+        $("body").css("background-image","url('background.png')");
         $(".serverMessage").text("");
         $(".header").hide();
         $(".footer").hide();
